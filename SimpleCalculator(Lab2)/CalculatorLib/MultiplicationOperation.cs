30f56608b2b35eddec99f363ce7b95fb8ec511c8
Insar Miftahov <insarmiftahov@yandex.ru>
2021-06-21 00:30:14 +0300

    Добавил операцию умножения

---
+++ SimpleCalculator(Lab2)/CalculatorLib/MultiplicationOperation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorLib
+{
+    public class MultiplicationOperation : IOperation
+    {
+        public string OperatorCode
+        {
+            get { return "*"; }
+        }
+
+        public int Apply(int operand1, int operand2)
+        {
+            return operand1 * operand2;
+        }
+    }
+}
